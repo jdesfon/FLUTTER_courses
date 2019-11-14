4199f2cbb8fef277e25c762b3ee2d5d4b781724a
jdesfon <jdesfon@gmail.com>
2019-11-14 20:53:22 +0100

    improvement: transform question from var to const

--- quizz_app/lib/main.dart
+++ quizz_app/lib/main.dart
@@ -25,7 +25,7 @@ class _MyAppState extends State<MyApp> {
 
   @override
   build(BuildContext context) {
-    var questions = [
+    const questions = const [
       {
         'questionText': 'What\'s your favorite color?',
         'answers' : ['red', 'blue', 'green', 'yellow'],
