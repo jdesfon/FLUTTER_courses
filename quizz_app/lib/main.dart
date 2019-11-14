9dd91a96cb8a16e9ca6bda815e37f1705cc89ead
jean-michel-dot-fox <jean-michel@foxintelligence.fr>
2019-11-14 10:21:35 +0100

    basic: private class/property

--- quizz_app/lib/main.dart
+++ quizz_app/lib/main.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() => MyAppState();
+  State<StatefulWidget> createState() => _MyAppState();
 }
 
-class MyAppState extends State<MyApp> {
-  var questionIndex = 0;
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
 
-  void answerQuestion() {
+  void _answerQuestion() {
     setState(() {
-      questionIndex == 0 ? questionIndex = 1 : questionIndex = 0;
+      _questionIndex == 0 ? _questionIndex = 1 : _questionIndex = 0;
     });
   }
 
@@ -30,18 +32,18 @@ class MyAppState extends State<MyApp> {
         ),
         body: Column(
           children: [
-            Text(questions[questionIndex]),
+            Question(questions[_questionIndex]),
             RaisedButton(
               child: Text('Answer 1'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 2'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
             RaisedButton(
               child: Text('Answer 3'),
-              onPressed: answerQuestion,
+              onPressed: _answerQuestion,
             ),
           ],
         ),
