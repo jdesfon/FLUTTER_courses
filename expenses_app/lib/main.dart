51ad28f10bddf0ded5043d42ec70e9a022ea88b5
jdesfon <jdesfon@gmail.com>
2019-12-08 16:55:39 +0100

    feat: Scaffold floating action button

--- expenses_app/lib/main.dart
+++ expenses_app/lib/main.dart
@@ -42,6 +42,11 @@ class MyHomePage extends StatelessWidget {
           ],
         ),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
     );
   }
 }
